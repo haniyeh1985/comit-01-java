f8d30b0f1ee4ef49b4d256e6e0c90035a4c4bdfe
Somayeh Abedi Isfahani <s.abedi.esfahani@gmail.com>
2023-02-26 13:46:40 -0600

    update Hello

---
+++ src/main/java/com/comit/course/Hello.java
@@ -0,0 +1,12 @@
+package com.comit.course;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=2;
+		System.out.println(x);
+
+	}
+
+}
